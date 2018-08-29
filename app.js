f58e8f4e3b683853435e4a64cd1562df2cad9bcb
Daniel Sullivan <daniel.sullivan@students.makeschool.com>
2018-08-29 14:33:10 -0700

    Added MongoDb and Review model

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express=require('express')
 const app= express()
-
+const mongoose = require('mongoose')
 // app.get('/',(req,res) => {
 //     res.send('Hello World!')
 // })
-
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+const Review = mongoose.model('Review', {
+  title: String
+});
 app.listen(3000,() => {
     console.log('App listening on port 3000!')
 })
@@ -14,18 +17,28 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
 
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Memaw reviews" }
+// ]
+
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 // INDEX
-app.get('/reviews', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
-})
+// app.get('/reviews', (req, res) => {
+//   res.render('reviews-index', { reviews: reviews });
+// })
