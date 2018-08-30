5b2c9c3e2d5cbb3e9d809e5f3e01c99212d0647c
Daniel Sullivan <daniel.sullivan@students.makeschool.com>
2018-08-30 12:10:55 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 // app.get('/',(req,res) => {
 //     res.send('Hello World!')
 // })
-//not sure if the var promise is necessary
+
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 const Review = mongoose.model('Review', {
   title: String,
@@ -51,12 +51,19 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`);
   }).catch((err) => {
     console.log(err.message);
   })
 })
-// app.post('/reviews', (req, res) => {
+
+app.get('/reviews/:id',(req,res) => {
+    Review.findById(req.params.id).then((review)=>{
+        res.render('reviews-show',{review:review})
+}).catch((err)=>{
+    console.log(err.message);
+})
+});// app.post('/reviews', (req, res) => {
 //   console.log(req.body);
 //   //put a dub slash here if need be
 //   //res.render('reviews-new', {});
