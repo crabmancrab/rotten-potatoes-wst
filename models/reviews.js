4a2cae09e37f714f1449cf5629954b8ba0bb8f9d
Daniel Sullivan <daniel.sullivan@students.makeschool.com>
2018-08-31 15:35:04 -0700

    hopefully works

--- models/reviews.js
+++ models/reviews.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/rotten-potatoes');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
 module.exports = mongoose.model('Review', {
   title: String,
   description: String,
