eb6536f8bc6c8dc4babb7508631eac9f0a6527eb
Daniel Sullivan <daniel.sullivan@students.makeschool.com>
2018-09-01 12:24:36 -0700

    lake

--- models/reviews.js
+++ models/reviews.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose')
-var MongoClient = require('mongodb').MongoClient
-mongoose.connect('process.env.MONGODB_URI' || 'mongodb://localhost:27017/rotten-potatoes');
-var url='process.env.MONGODB_URI' || 'mongodb://localhost:27017/rotten-potatoes'
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
 
-  db.close();
-});
+mongoose.connect('process.env.MONGODB_URI' || 'mongodb://localhost:27017/data/db');
+
+
 module.exports = mongoose.model('Review', {
   title: String,
   description: String,
