008899520892b728ee0286f4ef5cfebc71548624
Daniel Sullivan <daniel.sullivan@students.makeschool.com>
2018-09-02 12:47:31 -0700

    completed project

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -68,7 +68,14 @@ module.exports=function (app) {
     })
   })
 
-
+  app.delete('/reviews/comments/:id', function (req, res) {
+    console.log("DELETE comment")
+    Comment.findByIdAndRemove(req.params.id).then((comment) => {
+      res.redirect(`/reviews/${comment.reviewId}`);
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
 
 
 }
